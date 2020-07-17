45c791ed59f9bada72549dbf87c308326be798c7
Robin Lovelace <rob00x@gmail.com>
2020-07-17 15:14:00 +0100

    Update animation code

--- code/animation.R
+++ code/animation.R
@@ -5,67 +5,23 @@ tmap_mode("view")
 
 
 # Count length of journeys within each London Borough
+piggyback::pb_download_url("lads.Rds")
+# [1] "https://github.com/saferactive/saferactive/releases/download/0.1/lads.Rds"
+piggyback::pb_download("lads.Rds")
 lads = readRDS("lads.Rds")
-
-boroughs = c(
-  "Camden",
-  "Greenwich",
-  "Hackney",
-  "Hammersmith and Fulham",
-  "Islington",
-  "Kensington and Chelsea",
-  "Lambeth",
-  "Lewisham",
-  "Southwark",
-  "Tower Hamlets",
-  "Wandsworth",
-  "Westminster",
-  "Barking and Dagenham",
-  "Barnet",
-  "Bexley",
-  "Brent",
-  "Bromley",
-  "Croydon",
-  "Ealing",
-  "Enfield",
-  "Haringey",
-  "Harrow",
-  "Havering",
-  "Hillingdon",
-  "Hounslow",
-  "Kingston upon Thames",
-  "Merton",
-  "Newham",
-  "Redbridge",
-  "Richmond upon Thames",
-  "Sutton",
-  "Waltham Forest",
-  "City of London")
+boroughs = as.character(spData::lnd$NAME)
 
 lads = lads %>%
   filter(Name %in% boroughs)
 
 crashes_active_london = readRDS("crashes_active_london.Rds")
 
-# allyears = unique(crashes_active_london$year)
+crashes_active_london %>%
+  group_by()
 
-# for (i in (min(allyears)+2):(max(allyears)-2)){
-# years = ((i-2):(i+2))
-# crashes_i = filter(crashes_active_london, year %in% years)
-# crashes_per_borough_i = crashes_i %>%
-#   group_by(local_authority_district) %>%
-#   summarise(nam = paste("cycle_fatal",i, sep = ""),
-#             assign(nam, sum(cycle_fatal),
-#             cycle_adjusted_serious = sum(cycle_adjusted_serious),
-#             cycle_adjusted_slight = sum(cycle_adjusted_slight),
-#             cycle_KSI = cycle_fatal + cycle_adjusted_serious,
-#             ped_fatal = sum(ped_fatal),
-#             ped_adjusted_serious = sum(ped_adjusted_serious),
-#             ped_adjusted_slight = sum(ped_adjusted_slight),
-#             ped_KSI = ped_fatal + ped_adjusted_serious) %>%
-#   st_drop_geometry()
-# crashes_per_borough = inner_join(crashes_per_borough, crashes_per_borough_i, )
-# }
+summary(crashes_active_london$year)
+
+
 
 midyear = 2011
 years = ((midyear-2):(midyear+2))
@@ -85,7 +41,7 @@ crashes_m2014 = filter(crashes_active_london, year %in% years)
 
 midyear = 2015
 years = ((midyear-2):(midyear+2))
-crashes_m = filter(crashes_active_london, year %in% years)
+crashes_m2015 = filter(crashes_active_london, year %in% years)
 
 midyear = 2016
 years = ((midyear-2):(midyear+2))
@@ -177,8 +133,12 @@ crashes_per_borough = inner_join(lads, crashes_per_borough, by = c("Name" = "loc
 anim = tm_shape(crashes_per_borough) +
   tm_polygons("cycle_KSI", title = "Total cyclists KSI") +
   tm_facets(along = "year", free.coords = FALSE)
-tmap_animation(anim, filename = "anim.gif", delay = 25)
+tmap_animation(anim, filename = "anim.gif", delay = 200)
 
+browseURL("anim.gif")
+piggyback::pb_upload("anim.gif")
+piggyback::pb_download_url("anim.gif")
+# [1] "https://github.com/saferactive/saferactive/releases/download/0.1/anim.gif"
 
 urb_anim = tm_shape(world) + tm_polygons() +
   tm_shape(urban_agglomerations) + tm_dots(size = "population_millions") +
--- code/get-traffic-calming-data-from-osm.R
+++ code/get-traffic-calming-data-from-osm.R
@@ -31,3 +31,59 @@ file.remove("traffic_calming_osm_london.geojson")
 sf::st_write(traffic_calming_osm_london, "traffic_calming_osm_london.geojson")
 piggyback::pb_upload("traffic_calming_osm_london.geojson")
 piggyback::pb_download_url("traffic_calming_osm_london.geojson")
+
+traffic_calming_osm_london = sf::read_sf("https://github.com/saferactive/saferactive/releases/download/0.1/traffic_calming_osm_london.geojson")
+
+lnd = spData::lnd
+# names(lnd)
+lnd = lnd %>%
+  select(Borough = NAME)
+traffic_calming_osm_london = sf::st_join(traffic_calming_osm_london, lnd)
+
+
+tc_london_results = traffic_calming_osm_london %>%
+  sf::st_drop_geometry() %>%
+  mutate(
+    intervention = case_when(
+      traffic_calming == "bump;choker" ~ "bump",
+      traffic_calming == "choker;cushion" ~ "cushion",
+      traffic_calming == "choker;table" ~ "table",
+      traffic_calming == "table;choker" ~ "table",
+      grepl(pattern = "rumble|yes", x = traffic_calming) ~ "other",
+      TRUE ~ traffic_calming
+    )
+  ) %>%
+  group_by(intervention) %>%
+  summarise(
+    n = n(),
+    percent = n/nrow(traffic_calming_osm_london) * 100,
+    borough_with_most = Borough[which.max(n)]
+    )
+
+readr::write_csv(tc_london_results, "tc_london_results.csv")
+piggyback::pb_upload("tc_london_results.csv", repo = "saferactive/saferactive")
+piggyback::pb_download_url("tc_london_results.csv")
+
+# from cid
+
+remotes::install_github("PublicHealthDataGeek/CycleInfraLnd")
+library(CycleInfraLnd)
+
+cid_traffic_calming = sf::read_sf("https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/points/traffic_calming.json")
+
+summary(cid_traffic_calming)
+head(cid_traffic_calming)
+cid_london_results = cid_traffic_calming %>%
+  sf::st_drop_geometry() %>%
+  group_by(intervention) %>%
+  summarise(
+    n = n(),
+    percent = n/nrow(traffic_calming_osm_london) * 100,
+    borough_with_most = Borough[which.max(n)]
+  )
+
+readr::write_csv(cid_london_results, "cid_london_results.csv")
+piggyback::pb_upload("cid_london_results.csv", repo = "saferactive/saferactive")
+piggyback::pb_download_url("cid_london_results.csv")
+
+
