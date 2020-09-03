165ea74ec5e9ca2d635c4bd543b2506bea641b79
Robin Lovelace <rob00x@gmail.com>
2020-09-03 14:59:21 +0100

    Update with reproducible version of script

--- code/network-cleaning.R
+++ code/network-cleaning.R
@@ -3,23 +3,29 @@
 remotes::install_github("itsleeds/osmextract")
 library(osmextract)
 library(dplyr)
-osm_lines = oe_get("Greater London", extra_tags = c("ref", "maxspeed"))
-names(osm_lines)
-waterloo = osm_lines %>%
-  filter(name == "Waterloo Bridge")
+library(sf)
+# osm_lines = oe_get("Greater London", extra_tags = c("ref", "maxspeed"))
+# names(osm_lines)
+# waterloo = osm_lines %>%
+#   filter(name == "Waterloo Bridge")
+#
+# waterloo_buffer = waterloo %>%
+#   sf::st_union() %>%
+#   stplanr::geo_buffer(dist = 1000)
+#
+# mapview::mapview(waterloo_buffer)
+# osm_case_study = osm_lines[waterloo_buffer, , op = sf::st_within]
+# table(osm_case_study$highway)
+# osm_highways = osm_case_study %>%
+#   filter(!is.na(highway))
+#
+# saveRDS(osm_highways, "osm_highways.Rds")
+# piggyback::pb_upload("osm_highways.Rds")
+# piggyback::pb_download_url("osm_highways.Rds")
 
-waterloo_buffer = waterloo %>%
-  sf::st_union() %>%
-  stplanr::geo_buffer(dist = 1000)
+download.file("https://github.com/saferactive/saferactive/releases/download/0.1/osm_highways.Rds", "osm_highways.Rds")
 
-mapview::mapview(waterloo_buffer)
-osm_case_study = osm_lines[waterloo_buffer, , op = sf::st_within]
-table(osm_case_study$highway)
-osm_highways = osm_case_study %>%
-  filter(!is.na(highway))
-
-saveRDS(osm_highways, "osm_highways.Rds")
-piggyback::pb_upload("osm_highways.Rds")
+osm_highways = readRDS("osm_highways.Rds")
 
 osm_named_highways = osm_highways %>%
   filter(!is.na(name)) %>%
@@ -37,6 +43,3 @@ oe_download("Greater London", provider = "geofrabrik")
 # osmextract::oe_update(place = "Isle of Wight")
 # iow_roads = osmextract::oe_get("Isle of Wight")
 # city_centre = tmaptools::geocode_OSM()
-
-
-mapview::mapview(iow_roads[1:100, ])
