d64c5b4ad41c3cc4a652b35c5cec1e05cb94c0e1
Robin Lovelace <rob00x@gmail.com>
2020-09-30 20:20:29 +0100

    Add basic statistics - n repeats

--- code/dft-aadf-descriptive.R
+++ code/dft-aadf-descriptive.R
@@ -13,6 +13,34 @@ nrow(traffic_cyclable)
 length(unique(traffic_cyclable$count_point_id))
 # [1] 41980
 
-traffic_cyclable %>%
+summary_n_year = traffic_cyclable %>%
   group_by(year) %>%
-  summarise(n = n())
+  summarise(
+    n = n(),
+    n_ids = length(unique(count_point_id))
+    )
+summary(summary_n_year$n == summary_n_year$n_ids)
+# Mode    TRUE
+# logical      20
+
+ggplot(summary_n_year) +
+  geom_line(aes(year, n))
+ggsave("figures/aadf-counts-per-year.png")
+
+summary_n_year_id = traffic_cyclable %>%
+  group_by(year, count_point_id) %>%
+  summarise(
+    n = n()
+  )
+
+nrow(summary_n_year_id) == nrow(traffic_cyclable)
+# [1] TRUE # all variability captured in year and counter id
+
+summary_n_id = traffic_cyclable %>%
+  count(count_point_id)
+table(summary_n_id$n)
+
+summary_n_id %>%
+  ggplot() +
+  geom_histogram(aes(n), binwidth = 1)
+ggsave("figures/aadf-n-repeats-ids.png")
