55b822335a1df4d32fac7ec426777f3911e02dbb
Robin Lovelace <rob00x@gmail.com>
2020-12-02 13:43:30 +0000

    Update hisoric osm code

--- code/tests/historic-osm-test.R
+++ code/tests/historic-osm-test.R
@@ -5,14 +5,19 @@ remotes::install_github("itsleeds/osmextract")
 library(osmextract)
 library(tidyverse)
 library(sf)
-file.copy("~/hd/data/osm/internal/greater-london-160101-internal.osm.pbf", ".")
+
+osm_directory = "/home/bananafan/Downloads/"
+osm_directory = "/home/robin/hd/data/osm/internal/"
+
+# file.copy(file.path(osm_directory, "greater-london-160101-internal.osm.pbf"), ".")
 # fails:
 # lnd_2016 = osmextract::oe_read(file_path = "~/hd/data/osm/internal/greater-london-160101-internal.osm.pbf", layer = "lines")
 # lnd_2016 = osmextract::oe_read(file_path = "greater-london-160101-internal.osm.pbf", layer = "lines")
 # ?oe_read
+f = file.path(osm_directory, "greater-london-160101-internal.osm.pbf")
 extra_tags = c("maxspeed", "bicycle", "traffic_calming", "oneway", "lanes", "sidewalk", "ref", "lit", "foot", "motor_vehicle")
-lnd_2016 = osmextract::oe_read(file_path = "greater-london-160101-internal.osm.pbf", layer = "lines", extra_tags = extra_tags)
-lnd_2016$length = sf::st_length(lnd_2016)
+lnd_2016 = osmextract::oe_read(file_path = f, layer = "lines", extra_tags = extra_tags)
+lnd_2016$length = as.numeric(sf::st_length(lnd_2016))
 lnd = spData::lnd
 lnd$NAME
 mapview::mapview(lnd)
@@ -36,9 +41,10 @@ wf_2016s = wf_2016j %>%
 wf_2016s$year = 2016
 
 # do for 2018 data
-extra_tags = c("maxspeed", "bicycle", "traffic_calming", "oneway", "lanes", "sidewalk", "ref", "lit", "foot", "motor_vehicle")
-lnd_2018 = osmextract::oe_read(file_path = "/home/bananafan/Downloads/greater-london-180101.osm.pbf", layer = "lines", extra_tags = extra_tags)
-lnd_2018$length = sf::st_length(lnd_2018)
+# f = file.path(osm_directory, "greater-london-180101.osm.pbf")
+f = file.path(osm_directory, "greater-london-180101-internal.osm.pbf")
+lnd_2018 = osmextract::oe_read(file_path = f, layer = "lines", extra_tags = extra_tags)
+lnd_2018$length = as.numeric(sf::st_length(lnd_2018))
 
 wf_2018 = lnd_2018[wf_redbridge, , op = st_within]
 dim(wf_2018)
@@ -50,16 +56,30 @@ table(wf_2018$traffic_calming)
 
 wf_2018j = wf_2018 %>%
   st_join(x = ., y = wf_redbridge["NAME"])
-dim(wf_2018j) # bingo columsn = 22
+dim(wf_2018j) # bingo columns = 22, more data
 wf_2018s = wf_2018j %>%
   sf::st_drop_geometry() %>%
   group_by(NAME) %>%
   count(highway, wt = length)
 wf_2018s$year = 2018
 
-wf_2018s
+wf_yrs = rbind(wf_2018s, wf_2016s)
+wf_yrs$year = as.character(wf_yrs$year)
 
+ggplot(wf_yrs %>% filter(stringr::str_detect(string = highway, pattern = "cycle|res"))) +
+  geom_bar(aes(year, n, fill = year), stat = "identity") +
+  facet_grid(NAME ~ highway)
 
+wf_percent = wf_yrs %>%
+  group_by(NAME, highway) %>%
+  summarise(
+    relative_change = (percent_change = .data$n[.data$year == 2018] / .data$n[.data$year == 2016]),
+    percent_change = (relative_change - 1) * 100
+    )
+wf_percent
+ggplot(wf_percent %>% filter(stringr::str_detect(string = highway, pattern = "cycle|res"))) +
+  geom_bar(aes(highway, percent_change), stat = "identity") +
+  facet_grid(~NAME)
 # oe_download("isle of wight") # fails
 # u = oe_match("isle of wight")
 # oe_download(u$url, download_directory = ".")
