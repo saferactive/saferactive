1ade089422a74ff8286fceddfc4feb8ff1096d1a
Robin Lovelace <rob00x@gmail.com>
2020-12-16 09:25:56 +0000

    Start code to show absolute change in cycling

--- code/casualties-per-bkm-raster.R
+++ code/casualties-per-bkm-raster.R
@@ -58,4 +58,33 @@ tmap_mode("view")
 brks = c(0, 10, 100, 500, 1000, 5000, 10000)
 t1 = tm_shape(raster_i_bkm_london) + tm_raster(palette = "viridis", breaks = brks)
 
-raster_ksi_bkm_lnd =
+
+# crashes/uptake over time -------------------------------------------------------
+
+library(dplyr)
+u = "https://github.com/saferactive/saferactive/releases/download/0.1.2/gam-full-results-peak-grid.Rds"
+cycle_count_predictions = readRDS(url(u))
+nrow(cycle_count_predictions)
+ncell(raster_i_bkm_london)
+head(cycle_count_predictions$change_cycles)
+summary(cycle_count_predictions$change_cycles)
+summary(cycle_count_predictions$change_cycles[cycle_count_predictions$year == 2011])
+cycle_count_predictions_2012 = cycle_count_predictions %>%
+  filter(year == 2012)
+nrow(cycle_count_predictions_2012)
+raster_rnet_bicycle_london = raster::crop(raster_rnet_bicycle, london)
+
+r1 = raster_i_bkm_london
+values(r1) = NA
+coords = as.matrix(cycle_count_predictions_2012[1:2])
+
+head(cycle_count_predictions_2012)
+r2012 = rasterize(coords, r1, field = cycle_count_predictions_2012$change_cycles)
+plot(r2012)
+
+cycle_count_predictions_2013 = cycle_count_predictions %>%
+  filter(year == 2013)
+nrow(cycle_count_predictions_2013)
+
+r2013 = rasterize(coords, r1, field = cycle_count_predictions_2013$change_cycles)
+plot(r2013)
