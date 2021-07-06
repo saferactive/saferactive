f6875a7f6b3e67927953889dfc4076737dd0351f
Robin Lovelace <rob00x@gmail.com>
2021-07-06 14:16:09 +0100

    Add placeholder for work on #76

---
+++ code/tests/regional-uptake-nts.R
@@ -0,0 +1,6 @@
+# Aim get estimates of cycling uptake regionally from the NTS's summary datasets
+
+# See https://github.com/saferactive/saferactive/issues/76
+
+
+u = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/905969/nts9904.ods"
